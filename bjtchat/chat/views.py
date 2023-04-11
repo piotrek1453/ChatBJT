3758ba7fc4a47d899d9655d6fa2294bff7add87b
Rolf <121953201+rolf213@users.noreply.github.com>
2023-04-11 18:19:07 +0200

    działający czat na redis

---
+++ bjtchat/chat/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
+
+
+def index(request):
+    return render(request, "chat/index.html")
+
+def room(request, room_name):
+    return render(request, "chat/room.html", {"room_name": room_name})
